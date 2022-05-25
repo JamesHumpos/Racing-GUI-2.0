9c8157f2a00559c23072a9c5f44d098a0974e58e
JamesHumpos <72787617+JamesHumpos@users.noreply.github.com>
2022-05-25 08:34:58 +0100

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -1,15 +1,8 @@
 #! /usr/bin/python
 
-import git 
-import time as pause
+
 import subprocess
 import sys as sysname
-pause.sleep(5)
 
-repo = git.Repo('/home/james/GUIGIT')
-repo.git.reset('--hard')
-repo.remotes.origin.pull('master')
-
-pause.sleep(15)
 subprocess.Popen(['sudo','shutdown','-r','now'])
 sysname.exit(0)
