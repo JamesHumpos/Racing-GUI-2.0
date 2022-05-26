e95b7ead810d36d10f262c20ac79fb2fdcd7bad2
JamesHumpos <72787617+JamesHumpos@users.noreply.github.com>
2022-05-26 13:44:53 +0100

    Update autoupdate.py

--- autoupdate.py
+++ autoupdate.py
@@ -4,5 +4,10 @@
 import subprocess
 import sys as sysname
 
+## previously automatically updated script though Pi would kill original process when run on boot rather than from the main script. 
+## now restarts the pi, at which point the auto run script also updates the script on every restart
+
 subprocess.Popen(['sudo','shutdown','-r','now'])
+
+## probably superfluous will check when able to test run on machine
 sysname.exit(0)
