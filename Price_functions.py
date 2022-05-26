12a896f812dbe980ef7e9902eddba14565584218
JamesHumpos <72787617+JamesHumpos@users.noreply.github.com>
2022-05-26 10:47:09 +0100

    Update Price_functions.py

--- Price_functions.py
+++ Price_functions.py
@@ -1,42 +1,5 @@
 #! /usr/bin/python
 
-def MPChoiceBut():
-    global Choiceone
-    Choiceone = MPChoice.value
-    print(MPChoice.value)
-    MeetingTimeProd(Choiceone)
-
-def MeetingTimeProd(MeetingChosen):
-    RacesAPIChoice=RacesAPI[RacesAPI.course == MeetingChosen]
-    print(RacesAPIChoice)
-    print(MeetingChosen)
-    global RPChoice
-    if 'RPChoice' in globals():
-        RPChoice.destroy()
-        RPChoice = ButtonGroup(Boxraceprice2, options=RacesAPIChoice["time"],command=RPChoiceBut,width="fill",height="fill",align="right")
-        RPChoice.text_size = 30
-        for radio_button in RPChoice.children:
-            radio_button.tk.config(borderwidth=12)
-            radio_button.tk.config(bg="gainsboro")
-            radio_button.tk.config(relief="raised")
-    else:
-        RPChoice = ButtonGroup(Boxraceprice2, options=RacesAPIChoice["time"],command=RPChoiceBut,width="fill",height="fill",align="right")
-        RPChoice.text_size = 30
-        for radio_button in RPChoice.children:
-            radio_button.tk.config(borderwidth=12)
-            radio_button.tk.config(bg="gainsboro")
-            radio_button.tk.config(relief="raised")
-    #for times in RacesAPIChoice["time"]:
-        #RPChoice.append(times)
-    print(RacesAPIChoice["time"])
-    Window5_mprice.hide()
-    Window5a_rprice.show()
-    
-def RPChoiceBut():
-    global chosen_race
-    chosen_race = RacesAPI.loc[RacesAPI['time'] == RPChoice.value, 'id_race'].iloc[0]
-    changeschoice(chosen_race)
-
 def changeschoice(chosen_race):
     chosen_race = chosen_race
     #global chosen_race
@@ -98,30 +61,4 @@ def cleanracedata(racedata):
     Window5a_rprice.hide()
     showtable(horseraceall)
     
-def showtable(chosendata):
-    def HomePress2():
-        Window5b_rprice.hide()
-    Window5b_rprice = Window(app, title="Prices", visible=False, width=1500, height = 800)
-    Homebuttonpricer = PushButton(Window5b_rprice, command=HomePress2,text="HOME", width=80, height = 2)
-    Homebuttonpricer.text_color = "white"
-    Homebuttonpricer.bg = "red"
-    Homebuttonpricer.text_size = 20
-    style = ttk.Style()
-    style.configure("Treeview.Heading", highlightthickness=4, bd=0, font=('Calibri', 15))
-    tv = ttk.Treeview(Window5b_rprice.tk,style="Treeview.Heading")
-    tv["columns"]=list(chosendata.columns)
-    tv["show"]="headings"
-    for columns in tv["columns"]:
-        tv.heading(columns,text=columns)
-    tv.column("Horse",width=180)
-    tv.column("Age",width=50)
-    tv.column("Odds_Bet365",width=120)
-    tv.column("Weight",width=60)
-    tv.column("Running",width=70)
-    tv.column("Number",width=50)
-    tv.column("Jockey / Trainer",width=290)
-    chosendata_rows = chosendata.to_numpy().tolist()
-    for row in chosendata_rows:
-        tv.insert("","end",values=row)
-    Window5b_rprice.add_tk_widget(tv)
-    Window5b_rprice.show()
+
