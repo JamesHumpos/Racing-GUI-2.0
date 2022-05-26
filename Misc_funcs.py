3a0e5ea3db984d8c8b95e18c4464330c061ee8d5
JamesHumpos <72787617+JamesHumpos@users.noreply.github.com>
2022-05-26 10:49:27 +0100

    Update and rename PDF_functions.py to Misc_funcs.py

---
+++ Misc_funcs.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+import urllib.request
+
+
+def download_file(download_url, filename):
+    response = urllib.request.urlopen(download_url)    
+    file = open(filename, 'wb')
+    file.write(response.read())
+    file.close()
